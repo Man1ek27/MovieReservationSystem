25b632b02098642deeeeb2427fe5424eb22a1f2e
Mariusz Wątroba <mariusz2723@gmail.com>
2025-05-18 19:31:03 +0200

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 package src;
+
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application{
@@ -15,8 +19,18 @@ public class Main extends Application{
     @Override
     public void start(Stage stage){
 
-        Label label = new Label("Hellow Word");
-        Scene scene = new Scene(new StackPane(label), 500, 500);
+        Label label = new Label("Hello World");
+        label.setLayoutX(250);
+        label.setLayoutY(250);
+        label.setTextFill(Color.WHITE);
+        
+        Circle circ = new Circle(250, 250, 50 );
+        circ.setFill(Color.BLUE);
+
+        StackPane stack = new StackPane(circ, label);
+        
+        Scene scene = new Scene(stack, 500, 500);
+        stage.setTitle("Rezerwacja Miejsc");
         stage.setScene(scene);
         stage.show();
     }
