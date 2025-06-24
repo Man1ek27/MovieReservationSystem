4c74cee16ed350939ff6fc0f6e149b90c219bc1a
NorthernBardzioch <bardochman@student.agh.edu.pl>
2025-06-24 14:47:34 +0200

    KONIEC TEGO SHITU

--- src/controllers/PaymentController.java
+++ src/controllers/PaymentController.java
@@ -1,11 +1,16 @@
 package src.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.scene.paint.Color; // Dodaj import dla Color
+import javafx.stage.Stage;
 import org.java_websocket.client.WebSocketClient;
+import src.TheatreService;
 
 // TODO: Trzeba bedzie podpiac to potem do wszystkiego
 
@@ -77,6 +82,30 @@ public class PaymentController {
             if (wsClient != null) {
                 wsClient.reconnect();
             }
+        }
+    }
+
+    @FXML // Możesz oznaczyć jako FXML, jeśli będzie wywoływana z FXML (np. z przycisku Wróć)
+    private void returnToChooseTheatreView() {
+        try {
+            // Zamknij bieżące połączenie WebSocket, jeśli istnieje
+            closeWebSocket();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Resources/ChooseTheatreView.fxml"));
+            Parent root = loader.load();
+
+            // Jeśli ChooseTheatreController potrzebuje TheatreService, możesz je przekazać
+            ChooseTheatreController controller = loader.getController();
+            controller.setTheatreService(new TheatreService()); // Upewnij się, że TheatreService jest zainicjowany
+
+            // Pobierz bieżący Stage i ustaw nową scenę
+            Stage stage = (Stage) paymentMethodComboBox.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Wybierz teatr");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
